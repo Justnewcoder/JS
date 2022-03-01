57fc45b1b93e24219b6599fc4183f4ca4bca9a3a
Kirill Kisner <98433125+Justnewcoder@users.noreply.github.com>
2022-02-28 20:10:02 -0500

    loop_challange

---
+++ loop_challange/loop_challenge.js
@@ -0,0 +1,32 @@
+// for (var x=0; x<=20; x++) {
+    // if (x === 0) {
+            // console.log(x +  " is odd");
+    // }
+    // else if (x % 2 === 0) {
+            // console.log(x + " is odd");   
+    // }
+// }
+// End of challange 1-20 odd value--
+
+// for(var i =0; i <=100; i++){
+    // if(i % 3 == 0){
+        // console.log(i) 
+    // }
+// }
+// End of 3%100 challange--
+
+// var numbers = [4, 2.5, 1, -0.5, -2, -3.5]{
+
+// }
+        // end of what ever that is
+// function sumTo(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//     sum += i;
+//     }
+//     return sum;
+// }
+// console.log( sumTo(100));
+            // end of sigma
+
+// for(var product=)
