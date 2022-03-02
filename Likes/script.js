f173df7772d025cadc68814b0986ac5ef72c955b
Kirill Kisner <98433125+Justnewcoder@users.noreply.github.com>
2022-03-02 15:54:52 -0500

    homework

---
+++ Likes/script.js
@@ -0,0 +1,29 @@
+var count=0
+var countUp=document.querySelector("#countUp")
+console.log(countUp);
+
+function likeUp(){
+    count++
+    countUp.innerText=count
+    console.log(count);
+}
+
+var count=0
+var counter=document.querySelector("#counter")
+console.log(counter);
+
+function justNew(){
+    count++
+    counter.innerText=count
+    console.log(count);
+}
+
+var count=0
+var countedUp=document.querySelector("#countedUp")
+console.log(countedUp);
+
+function likedUp(){
+    count++
+    countedUp.innerText=count
+    console.log(count);
+}
