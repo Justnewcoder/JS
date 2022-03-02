07a724967f1b07835392f88da5b8bc29ad2e553b
Kirill Kisner <98433125+Justnewcoder@users.noreply.github.com>
2022-03-01 23:43:34 -0500

    homeworks

---
+++ buttonClicker/script.js
@@ -0,0 +1,18 @@
+
+function sayLogout(element){
+    element.innerText="log Out"
+}
+function removebutton(element){
+    element.remove()
+}
+// log in/out
+
+function showAlert() {
+    var myText = "Ninja got a like!";
+    alert (myText);
+}
+// both ninjas like msg
+
+function hideButton(btn3){
+    btn3.style.display = 'none';
+}
